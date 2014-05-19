05472c0d2eb20c991f58a38cf3a6b06bc1f66fe5
Chris Fusting <cfusting@gmail.com>
2014-05-19 10:13:42 -0400

    refining algorithm

---
+++ quicksort.js
@@ -0,0 +1,54 @@
+function qs(a, l, r, selectPivot) {
+    if (r - l <= 0) return(0);
+    var cp = r - l - 1;
+    var p = selectPivot(a, l, r);
+    console.log('Pivot value: ' + a[p]);
+    var m = prt(a, l, r, p);
+    cp = cp + qs(a, l, m, selectPivot);
+    cp = cp + qs(a, m + 1, r, selectPivot);
+    return(cp);
+}
+
+function prt(a, l, r, p) {
+    console.log('Parting: ' + a.slice(l, r));
+    var t = a[l];
+    a[l] = a[p];
+    a[p] = t;
+    var pivotValue = a[l];
+    var i = l + 1;
+    for (var q = i; q < r; q++) {
+        if (a[q] < pivotValue) {
+            console.log('Swapping ' + a[q] + ' and ' + a[i]);
+            var t = a[i];
+            a[i] = a[q];
+            a[q] = t;
+            i = i + 1;
+        }
+    }
+    t = a[l];
+    a[l] = a[i - 1];
+    a[i - 1] = t;
+    console.log('Parted: ' + a.slice(l, r));
+    return(i - 1);
+}
+
+function selectFirstPivot(a, l, r) {
+    return(l);
+}
+
+function selectLastPivot(a, l, r) {
+    return(r - 1);
+}
+
+function selectMedianPivot(a, l r) {
+    var v = a.splice(l, r);
+    
+}
+
+var a = [3,9,8,4,6,10,2,5,7,1];
+var csv = require('fast-csv');
+csv.fromPath('qs10.txt').on('record', function(data) { a.push(parseInt(data)) });
+console.log(a);
+var cp = qs(a, 0, a.length, selectLastPivot);
+console.log(a);
+console.log(cp);
